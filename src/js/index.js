15bb0af9932e7a0224eca56ba9303244780fc657
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2020-06-19 23:45:34 +0200

    add history table

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,10 @@ registerSW();
 const plus = document.querySelector('.button__add--js');
 const minus = document.querySelector('.button__remove--js');
 const number = document.querySelector('.number--js');
+const historyButton = document.querySelector('.button__history--js');
+const history = document.querySelector('.history--js');
+const closeIcon = document.querySelector('.history__icon');
+const table = document.querySelector('.tbody--js');
 
 const glass = localStorage.getItem('glass');
 
@@ -29,6 +33,7 @@ plus.addEventListener('click', () => {
         count = count + 1;
         number.innerHTML = count;
         localStorage.setItem('glass', count);
+        location.reload();
     }
 })
 
@@ -39,5 +44,26 @@ minus.addEventListener('click', () => {
         count = count - 1;
         number.innerHTML = count;
         localStorage.setItem('glass', count);
+        location.reload();
     }
 })
+
+
+historyButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  history.classList.add('history--visible');
+});
+
+closeIcon.addEventListener('click', (e) => {
+  e.preventDefault();
+  history.classList.remove('history--visible');
+});
+
+
+if (table) {
+      let value = localStorage.getItem(key, count);
+        table.innerHTML += `<tr class="tbody__row">
+                              <td class="tbody__data">${key}</td>
+                              <td class="tbody__data">${count}</td>
+                            </tr>`;
+  }
