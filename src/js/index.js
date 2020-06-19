a9411b10754b742e6220ccc899020cd95479c6ca
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2020-06-19 13:57:38 +0200

    function in js finish

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,39 @@ import '../scss/main.scss';
 // import {registerSW} from './pwa.js';
 // registerSW();
 
-/* place your code below */
 
-console.log('HELLO 🚀')
+const plus = document.querySelector('.button__add--js');
+const minus = document.querySelector('.button__remove--js');
+const number = document.querySelector('.number--js');
+const key = new Date().toISOString().slice(0, 10);
+let count = 0;
+const glass = localStorage.getItem('glass');
+
+
+if (glass) {
+    number.innerHTML = glass;
+    count = parseInt(glass);
+} else {
+    number.innerHTML = count;
+}
+
+
+plus.addEventListener('click', () => {
+    if (count >= 99) {
+        count = 99;
+    } else {
+        count = count + 1;
+        number.innerHTML = count;
+        localStorage.setItem('glass', count);
+    }
+})
+
+minus.addEventListener('click', () => {
+    if (count <= 0) {
+        count = 0;
+    } else {
+        count = count - 1;
+        number.innerHTML = count;
+        localStorage.setItem('glass', count);
+    }
+})
